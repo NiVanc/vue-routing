c4d55275d96e1888f772f791ceb1b2c5274e1f46
NiVanc <nico@izebra.be>
2020-04-22 01:20:48 +0200

    Add beforeLeave guards

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -4,6 +4,7 @@
     <hr />
     <p>Locale: {{ $route.query.locale }}</p>
     <p>Analytics: {{ $route.query.q }}</p>
+    <button class="btn btn-primary" @click="confirmed = true">Confirm</button>
     <div style="height: 1200px"></div>
     <p id="data">Here is some additional data</p>
   </div>
@@ -11,9 +12,25 @@
 
 <script>
 export default {
+  data() {
+    return {
+      confirmed: false
+    };
+  },
   beforeRouteEnter(to, from, next) {
     console.log("beforeRouteEnter");
     next();
+  },
+  beforeRouteLeave(to, from, next) {
+    if (this.confirmed) {
+      next();
+    } else {
+      if (confirm("Are you sure?")) {
+        next();
+      } else {
+        next(false);
+      }
+    }
   }
 };
 </script>
