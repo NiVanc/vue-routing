902fe5af428c8067a529a09cea443d794c40a78a
NiVanc <nico@izebra.be>
2020-04-21 23:18:55 +0200

    Navigate from code

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>The User Page</h1>
+  <div>
+    <h1>The User Page</h1>
+    <hr />
+    <button class="btn btn-primary" @click="navigateToTome">Go to home</button>
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    navigateToTome() {
+      this.$router.push("/");
+    }
+  }
+};
+</script>
