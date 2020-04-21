2485ea56c57ef717772bc91f3540b4ecdc20efa5
NiVanc <nico@izebra.be>
2020-04-22 00:30:45 +0200

    Add multiple router views

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,25 @@
 import Home from "./components/Home.vue";
+import Header from "./components/Header.vue";
 import User from "./components/user/User.vue";
 import UserStart from "./components/user/UserStart.vue";
 import UserDetail from "./components/user/UserDetail.vue";
 import UserEdit from "./components/user/UserEdit.vue";
 
 export const routes = [
-  { path: "", component: Home, name: "home" },
+  {
+    path: "",
+    name: "home",
+    components: {
+      default: Home,
+      "header-top": Header,
+    },
+  },
   {
     path: "/user",
-    component: User,
+    components: {
+      default: User,
+      "header-bottom": Header,
+    },
     props: true,
     children: [
       { path: "", component: UserStart },
