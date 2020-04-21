134d4a9b8844cf0c34cbaf13b70c76756736861a
NiVanc <nico@izebra.be>
2020-04-22 01:34:58 +0200

    Lazy load a group

--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,23 @@ import Home from "./components/Home.vue";
 import Header from "./components/Header.vue";
 
 const User = (resolve) => {
-  require.ensure(["./components/user/User.vue"], () => {
-    resolve(require("./components/user/User.vue"));
-  });
+  require.ensure(
+    ["./components/user/User.vue"],
+    () => {
+      resolve(require("./components/user/User.vue"));
+    },
+    "user"
+  );
 };
 
 const UserStart = (resolve) => {
-  require.ensure(["./components/user/UserStart.vue"], () => {
-    resolve(require("./components/user/UserStart.vue"));
-  });
+  require.ensure(
+    ["./components/user/UserStart.vue"],
+    () => {
+      resolve(require("./components/user/UserStart.vue"));
+    },
+    "user"
+  );
 };
 
 const UserDetail = (resolve) => {
